8e5bafc0bb9c715c3b9dca494cddd67a4bd561e2
ARTS-str <magninfacundo@gmail.com>
2025-09-18 15:23:04 -0300

    Agregar calificaciones

--- js/product-info.js
+++ js/product-info.js
@@ -1,4 +1,5 @@
 const productID = localStorage.getItem("productID");
+const commentContainer = document.getElementById("comments");
 let productData = {};
 let currentIndex = 0;
 
@@ -66,4 +67,72 @@ document.addEventListener("DOMContentLoaded", () => {
             console.error("Error al obtener la info del producto"); //si no puede cargar la function, muestra este error en la pantalla
         }
     });
+
+    getJSONData(PRODUCT_INFO_COMMENTS_URL + productID + ".json").then(resultObj => { //Fetch a los comentarios del producto
+        if (resultObj.status === "ok") { 
+            resultObj.data.forEach(comment => { //Para cada comentario
+                addComment(comment); //Agregarlo a la lista
+            });
+        } else {
+            console.error("Error al obtener los comentarios del producto"); //Si no puede cargar la function, muestra este error en la pantalla
+        }
+    });
 });
+
+/**
+ * Formato obligatorio del commentObj: 
+ * {
+ *  user: "pepito",
+ *  score: 1, (numero entero del 1 al 5)
+ *  description: "no me gusto la silla",
+ *  dateTime: yyyy-mm-dd hh:mm:ss
+ * }
+ */
+
+function calculateStars(commentObj) {
+    let starsArray = [];
+    for (let i = 0; i < commentObj.score; i++) {  //Por cada punto
+        starsArray.push('<i class="bi bi-star-fill"></i>'); //Agregar estrella llena
+    }
+    if (starsArray.length != 5) { //Si no llega a 5 puntos
+        for (let i = starsArray.length; i < 5; i++) {
+            starsArray.push('<i class="bi bi-star"></i>'); //Agregar estrella vacía
+        }
+    }
+    let stars = starsArray.join(" "); //Transformar array a string
+    return stars; //Devolver la string con las estrellas correctas segun la puntuacion
+}
+
+function addComment(commentObj) {
+    let stars = calculateStars(commentObj); //String con las estrellas del producto
+
+    //Agregar al container de comentarios uno nuevo
+    commentContainer.innerHTML += `
+    <li>
+      <span>
+        <b id="username">${commentObj.user}</b>
+        <span id="stars">
+            ${stars}
+        </span>
+      </span>
+      <p class="description">${commentObj.description}</p>
+      <p class="time">${formatTimestamp(commentObj.dateTime)}</p>
+    </li>
+    `
+}
+
+function formatTimestamp(ts) {
+  const date = new Date(ts); // "yyyy-mm-dd hh:mm:ss"
+
+  const options = {
+    weekday: 'short',   // ej "jue"
+    day: '2-digit',    // ej "18"
+    month: 'short',     // ej "sept."
+    year: 'numeric',   // ej "2025"
+    hour: '2-digit',   // ej "13"
+    minute: '2-digit', // ej "45"
+    hour12: false      // formato 24h
+  };
+
+  return date.toLocaleString(undefined, options).replace(',', ''); //Devolver la timestamp en formato "Dia dd Mes, yyy, hh:mm"
+}
