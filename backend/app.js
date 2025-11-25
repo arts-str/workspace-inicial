fa2e67bc4893752a758eeb566a363c4039defc3a
Cristopher Olivera <crisolapofder@gmail.com>
2025-11-25 16:21:21 -0300

    middleware

--- backend/app.js
+++ backend/app.js
@@ -14,11 +14,24 @@ app.use(cors());
 
 app.use(express.json()); // Permite que el servidor analice el cuerpo de las peticiones como JSON
 
-app.use("/products", productRouter);
+app.use("/api", (req, res, next) => {
+  try {
+    const decoded = jwt.verify(req.headers["access-token"], SECRET_KEY);
+    console.log(decoded);
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Usuario no autorizado" });
+  }
+});
 
-app.use("/cart", cartRouter);
+app.use("/api/products", productRouter);
 
-app.use("/category", categoryRouter);
+app.use("/api/cart", cartRouter);
+
+app.use("/api/category", categoryRouter);
+
+
+
 
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
